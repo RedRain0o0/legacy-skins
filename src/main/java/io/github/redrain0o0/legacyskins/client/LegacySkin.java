c8c3ccd3837bc1b8f228f79c556152fe80ba9fd7
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-08-13 20:57:58 +1000

    rename to `texture`

--- src/main/java/io/github/redrain0o0/legacyskins/client/LegacySkin.java
+++ src/main/java/io/github/redrain0o0/legacyskins/client/LegacySkin.java
@@ -4,8 +4,8 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.resources.ResourceLocation;
 
-public record LegacySkin(ResourceLocation location) {
+public record LegacySkin(ResourceLocation texture) {
 	public static final Codec<LegacySkin> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-			ResourceLocation.CODEC.fieldOf("location").forGetter(LegacySkin::location)
+			ResourceLocation.CODEC.fieldOf("texture").forGetter(LegacySkin::texture)
 	).apply(instance, LegacySkin::new));
 }
