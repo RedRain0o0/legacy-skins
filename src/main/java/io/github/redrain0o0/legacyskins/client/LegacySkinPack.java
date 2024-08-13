de6084708b9e1296efaeeef9ae4e2c489f02aed4
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-08-13 20:50:20 +1000

    codecize `LegacySkinPack`

--- src/main/java/io/github/redrain0o0/legacyskins/client/LegacySkinPack.java
+++ src/main/java/io/github/redrain0o0/legacyskins/client/LegacySkinPack.java
@@ -1,7 +1,12 @@
 package io.github.redrain0o0.legacyskins.client;
 
 import com.google.gson.JsonObject;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.JsonOps;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import io.github.redrain0o0.legacyskins.Legacyskins;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.ComponentSerialization;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
@@ -15,9 +20,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public record LegacySkinPack(Component buttonName, ResourceLocation icon, ResourceLocation skins) {
+public record LegacySkinPack(ResourceLocation icon, List<ResourceLocation> skins) {
     public static final List<LegacySkinPack> list = new ArrayList<>();
     private static final String PACKS = "skin_packs.json";
+	public static final Codec<LegacySkinPack> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+			ResourceLocation.CODEC.fieldOf("icon").forGetter(LegacySkinPack::icon),
+			Codec.list(ResourceLocation.CODEC).fieldOf("skins").forGetter(LegacySkinPack::skins)
+	).apply(instance, LegacySkinPack::new));
+
     public static class Manager extends SimplePreparableReloadListener<List<LegacySkinPack>> {
         @Override
         protected List<LegacySkinPack> prepare(ResourceManager resourceManager, ProfilerFiller profilerFiller) {
@@ -28,9 +38,12 @@ public record LegacySkinPack(Component buttonName, ResourceLocation icon, Resour
                         BufferedReader bufferedReader = r.openAsReader();
                         JsonObject obj = GsonHelper.parse(bufferedReader);
                         obj.asMap().forEach((s,element)->{
+							// yikes!
                             if (element instanceof JsonObject tabObj) {
-                                packs.add(new LegacySkinPack(Component.translatable(s),ResourceLocation.parse(GsonHelper.getAsString(tabObj,"icon")),ResourceLocation.parse(GsonHelper.getAsString(tabObj,"skins"))));
-                            }
+                                //packs.add(new LegacySkinPack(Component.translatable(s),ResourceLocation.parse(GsonHelper.getAsString(tabObj,"icon")),ResourceLocation.parse(GsonHelper.getAsString(tabObj,"skins"))));
+								LegacySkinPack deserialized = CODEC.parse(JsonOps.INSTANCE, tabObj).resultOrPartial(Legacyskins.LOGGER::error).orElseThrow();
+								packs.add(deserialized);
+							}
                         });
                         bufferedReader.close();
                     } catch (IOException var8) {
@@ -44,7 +57,7 @@ public record LegacySkinPack(Component buttonName, ResourceLocation icon, Resour
         @Override
         protected void apply(List<LegacySkinPack> object, ResourceManager resourceManager, ProfilerFiller profilerFiller) {
             list.clear();
-            list.addAll(object);;
+            list.addAll(object);
         }
     }
 }
