27c5199f72d44aedaf34835d83decc7475383ef7
RedRain0o0 <96890698+RedRain0o0@users.noreply.github.com>
2024-08-15 07:27:22 -0400

    redid the gui to account for the see-thru part

--- src/main/java/io/github/redrain0o0/legacyskins/util/LegacySkinSprites.java
+++ src/main/java/io/github/redrain0o0/legacyskins/util/LegacySkinSprites.java
@@ -5,4 +5,7 @@ import net.minecraft.resources.ResourceLocation;
 
 public class LegacySkinSprites {
 	public static final ResourceLocation SKIN_BOX = ResourceLocation.tryBuild(Legacyskins.MOD_ID, "tiles/skin_box");
+	public static final ResourceLocation SKIN_PANEL = ResourceLocation.tryBuild(Legacyskins.MOD_ID, "tiles/skin_panel");
+	public static final ResourceLocation PANEL_FILLER = ResourceLocation.tryBuild(Legacyskins.MOD_ID, "tiles/panel_filler");
+	public static final ResourceLocation PACK_NAME_BOX = ResourceLocation.tryBuild(Legacyskins.MOD_ID, "tiles/pack_name_box");
 }
