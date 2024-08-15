aa0a961829c6eb7d018f8edcca24f4415cd569d3
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-08-15 22:59:54 +1000

    Update AnimationEngineMixin.java

--- src/main/java/io/github/redrain0o0/legacyskins/mixin/AnimationEngineMixin.java
+++ src/main/java/io/github/redrain0o0/legacyskins/mixin/AnimationEngineMixin.java
@@ -1,10 +1,31 @@
 package io.github.redrain0o0.legacyskins.mixin;
 
+import com.tom.cpm.shared.MinecraftClientAccess;
+import com.tom.cpm.shared.animation.AnimationEngine;
+import com.tom.cpm.shared.network.NetHandler;
+import com.tom.cpm.shared.network.ServerCaps;
+import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(AnimationEngineMixin.class)
+@Mixin(value = AnimationEngine.class, remap = false)
 public class AnimationEngineMixin {
-	void prepareAnimations() {
+	//@Expression("")
+	// MinecraftClientAccess.get().getNetHandler().hasServerCap(ServerCaps.GESTURES)
+	@Redirect(method = "prepareAnimations", at = @At(value = "INVOKE", target = "Lcom/tom/cpm/shared/network/NetHandler;hasServerCap(Lcom/tom/cpm/shared/network/ServerCaps;)Z"))
+	boolean prepareAnimations(NetHandler<?, ?, ?> instance, ServerCaps cap) {
+		if (Minecraft.getInstance().getConnection() != null) {
+			return instance.hasServerCap(cap);
+		}
+		return true;
+	}
 
+	@Redirect(method = "handleAnimation", at = @At(value = "INVOKE", target = "Lcom/tom/cpm/shared/network/NetHandler;hasServerCap(Lcom/tom/cpm/shared/network/ServerCaps;)Z"))
+	boolean handleAnimation(NetHandler<?, ?, ?> instance, ServerCaps cap) {
+		if (Minecraft.getInstance().getConnection() != null) {
+			return instance.hasServerCap(cap);
+		}
+		return true;
 	}
 }
