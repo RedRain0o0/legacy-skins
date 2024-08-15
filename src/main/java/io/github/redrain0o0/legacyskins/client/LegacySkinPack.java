52ea488365a477d0b244c4c9e82b408ae739ea49
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-08-15 11:04:41 +1000

    change packs to use a map

--- src/main/java/io/github/redrain0o0/legacyskins/client/LegacySkinPack.java
+++ src/main/java/io/github/redrain0o0/legacyskins/client/LegacySkinPack.java
@@ -20,23 +20,25 @@ import wily.legacy.util.JsonUtil;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
 public record LegacySkinPack(ResourceLocation icon, List<LegacySkin> skins) {
-    public static final List<LegacySkinPack> list = new ArrayList<>();
+    public static final Map<String, LegacySkinPack> list = new LinkedHashMap<>();
     private static final String PACKS = "skin_packs.json";
 	public static final Codec<LegacySkinPack> CODEC = RecordCodecBuilder.create(instance -> instance.group(
 			ResourceLocation.CODEC.fieldOf("icon").forGetter(LegacySkinPack::icon),
 			Codec.list(LegacySkin.CODEC).fieldOf("skins").forGetter(LegacySkinPack::skins)
 	).apply(instance, LegacySkinPack::new));
 
-    public static class Manager implements SimpleResourceReloadListener<List<LegacySkinPack>> {
+    public static class Manager implements SimpleResourceReloadListener<Map<String, LegacySkinPack>> {
 		@Override
-		public CompletableFuture<List<LegacySkinPack>> load(ResourceManager resourceManager, ProfilerFiller profiler, Executor executor) {
+		public CompletableFuture<Map<String, LegacySkinPack>> load(ResourceManager resourceManager, ProfilerFiller profiler, Executor executor) {
 			return CompletableFuture.supplyAsync(() -> {
-				List<LegacySkinPack> packs = new ArrayList<>();
+				Map<String, LegacySkinPack> packs = new LinkedHashMap<>();
 				JsonUtil.getOrderedNamespaces(resourceManager).forEach(name->{
 					resourceManager.getResource(ResourceLocation.tryBuild(name, PACKS)).ifPresent(r->{
 						try {
@@ -47,7 +49,7 @@ public record LegacySkinPack(ResourceLocation icon, List<LegacySkin> skins) {
 								if (element instanceof JsonObject tabObj) {
 									//packs.add(new LegacySkinPack(Component.translatable(s),ResourceLocation.parse(GsonHelper.getAsString(tabObj,"icon")),ResourceLocation.parse(GsonHelper.getAsString(tabObj,"skins"))));
 									LegacySkinPack deserialized = CODEC.parse(JsonOps.INSTANCE, tabObj).resultOrPartial(Legacyskins.LOGGER::error).orElseThrow();
-									packs.add(deserialized);
+									packs.put(s, deserialized);
 								}
 							});
 							bufferedReader.close();
@@ -61,9 +63,9 @@ public record LegacySkinPack(ResourceLocation icon, List<LegacySkin> skins) {
 		}
 
 		@Override
-		public CompletableFuture<Void> apply(List<LegacySkinPack> data, ResourceManager manager, ProfilerFiller profiler, Executor executor) {
+		public CompletableFuture<Void> apply(Map<String, LegacySkinPack> data, ResourceManager manager, ProfilerFiller profiler, Executor executor) {
 			list.clear();
-			list.addAll(data);
+			list.putAll(data);
 			return CompletableFuture.completedFuture(null);
 		}
 
