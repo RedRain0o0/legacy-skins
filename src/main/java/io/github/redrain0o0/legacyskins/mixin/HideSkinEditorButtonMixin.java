2891d854017b702e8fd684259cee9d0950e21275
RedRain0o0 <96890698+RedRain0o0@users.noreply.github.com>
2024-08-17 15:58:57 -0400

    removed the mixin for removing the open skin editor button

--- src/main/java/io/github/redrain0o0/legacyskins/mixin/HideSkinEditorButtonMixin.java
+++ src/main/java/io/github/redrain0o0/legacyskins/mixin/HideSkinEditorButtonMixin.java
@@ -6,13 +6,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = CustomPlayerModelsClient.class, remap = false)
+//@Mixin(value = CustomPlayerModelsClient.class, remap = false)
 public class HideSkinEditorButtonMixin {
 
-    @Inject(method = "lambda$onInitializeClient$5(Lnet/minecraft/client/Minecraft;Lnet/minecraft/client/gui/screens/Screen;II)V", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/api/event/Event;register(Ljava/lang/Object;)V", shift = At.Shift.AFTER), cancellable = true)
-    private static void RemoveEditorButton(CallbackInfo ci) {
-        ci.cancel();
-    }
+    //@Inject(method = "lambda$onInitializeClient$5(Lnet/minecraft/client/Minecraft;Lnet/minecraft/client/gui/screens/Screen;II)V", at = @At(value = "INVOKE", target = "Lnet/fabricmc/fabric/api/event/Event;register(Ljava/lang/Object;)V", shift = At.Shift.AFTER), cancellable = true)
+    //private static void RemoveEditorButton(CallbackInfo ci) {
+    //    ci.cancel();
+    //}
 
 }
 
