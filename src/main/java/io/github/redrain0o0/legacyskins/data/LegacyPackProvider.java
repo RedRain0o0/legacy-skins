27eca97bee3f4ef89430be2682e2681908a28aa4
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-08-17 22:38:45 +1000

    make data generator not re-order stuff

--- src/main/java/io/github/redrain0o0/legacyskins/data/LegacyPackProvider.java
+++ src/main/java/io/github/redrain0o0/legacyskins/data/LegacyPackProvider.java
@@ -1,16 +1,26 @@
 package io.github.redrain0o0.legacyskins.data;
 
+import com.google.common.hash.Hashing;
+import com.google.common.hash.HashingOutputStream;
 import com.google.gson.JsonElement;
+import com.google.gson.stream.JsonWriter;
 import com.mojang.serialization.JsonOps;
 import io.github.redrain0o0.legacyskins.Legacyskins;
 import io.github.redrain0o0.legacyskins.client.LegacySkinPack;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.minecraft.Util;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.CachedOutput;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 import java.util.LinkedHashMap;
 import java.util.TreeMap;
 import java.util.concurrent.CompletableFuture;
@@ -32,8 +42,37 @@ public abstract class LegacyPackProvider implements DataProvider {
 			addPacks(new InternalPackBuilder(packs));
 			JsonElement element = LegacySkinPack.MAP_CODEC.encodeStart(JsonOps.INSTANCE, packs).resultOrPartial(Legacyskins.LOGGER::error).orElseThrow();
 			System.out.println(element);
-			return DataProvider.saveStable(cachedOutput, element, dataOutput.getOutputFolder(PackOutput.Target.RESOURCE_PACK).resolve(dataOutput.getModId()).resolve("skin_packs.json"));
+			return saveNotStable(cachedOutput, element, dataOutput.getOutputFolder(PackOutput.Target.RESOURCE_PACK).resolve(dataOutput.getModId()).resolve("skin_packs.json"));
 		});
+	}
+
+	static CompletableFuture<?> saveNotStable(CachedOutput cachedOutput, JsonElement jsonElement, Path path) {
+		return CompletableFuture.runAsync(() -> {
+			try {
+				ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+				HashingOutputStream hashingOutputStream = new HashingOutputStream(Hashing.sha1(), byteArrayOutputStream);
+				JsonWriter jsonWriter = new JsonWriter(new OutputStreamWriter(hashingOutputStream, StandardCharsets.UTF_8));
+
+				try {
+					jsonWriter.setSerializeNulls(false);
+					jsonWriter.setIndent("  ");
+					GsonHelper.writeValue(jsonWriter, jsonElement, null);
+				} catch (Throwable var9) {
+					try {
+						jsonWriter.close();
+					} catch (Throwable var8) {
+						var9.addSuppressed(var8);
+					}
+
+					throw var9;
+				}
+
+				jsonWriter.close();
+				cachedOutput.writeIfNeeded(path, byteArrayOutputStream.toByteArray(), hashingOutputStream.hash());
+			} catch (IOException var10) {
+				LOGGER.error("Failed to save file to {}", path, var10);
+			}
+		}, Util.backgroundExecutor());
 	}
 
 	public abstract void addPacks(PackBuilder builder);
