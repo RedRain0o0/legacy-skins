a4568f6839ec9ac56e40511110c20182610f0f90
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-08-18 23:31:42 +1000

    rename to migrator

---
+++ src/main/java/io/github/redrain0o0/legacyskins/schema/Migrator.java
@@ -0,0 +1,67 @@
+package io.github.redrain0o0.legacyskins.schema;
+
+import com.mojang.serialization.Dynamic;
+import com.mojang.serialization.DynamicOps;
+import com.mojang.serialization.MapLike;
+import io.github.redrain0o0.legacyskins.Legacyskins;
+import io.github.redrain0o0.legacyskins.schema.fixer.Fixer;
+import io.github.redrain0o0.legacyskins.schema.fixer.ScreenTypeFixer;
+import io.github.redrain0o0.legacyskins.schema.fixer.SkinToCurrentSkinFixer;
+
+import java.util.List;
+
+public class Migrator {
+	private final List<Fixer> fixers;
+	public static final Migrator CONFIG_FIXER = new Migrator(
+			List.of(
+					new ScreenTypeFixer(),
+					new SkinToCurrentSkinFixer()
+			), 1001, 1002
+	);
+
+	public static final Migrator SKIN_PACKS_FIXER = new Migrator(
+			List.of(),
+			1, 1
+	);
+
+	private final int oldestSupportedVersion;
+	private final int schemaVersion;
+
+	public Migrator(List<Fixer> fixers, int oldestSupportedVersion, int schemaVersion) {
+		this.fixers = fixers;
+		this.oldestSupportedVersion = oldestSupportedVersion;
+		this.schemaVersion = schemaVersion;
+	}
+
+	public <T> Dynamic<T> fix(Dynamic<T> element) {
+		int schemaVersion = element.get("schemaVersion").asInt(1001);
+		if (schemaVersion < oldestSupportedVersion) throw new UnsupportedOperationException();
+		element.remove("schemaVersion");
+		if (schemaVersion > oldestSupportedVersion) throw new UnsupportedOperationException();
+		if (schemaVersion == this.schemaVersion) return element;
+		Dynamic<T> dynamic = element;
+		for (Fixer fixer : fixers) {
+			if (fixer.minApplicable >= schemaVersion) {
+				dynamic = fixer.fix(dynamic);
+			}
+		}
+		return dynamic;
+	}
+
+	public <T> T fix(DynamicOps<T> ops, T value) {
+		return fix(new Dynamic<>(ops, value)).getValue();
+	}
+
+	public <T> Dynamic<T> addSchemaVersion(Dynamic<T> dynamic) {
+		DynamicOps<T> ops = dynamic.getOps();
+		Dynamic<T> newDynamic = new Dynamic<>(ops, ops.emptyMap());
+		newDynamic = newDynamic.set("schemaVersion", dynamic.createInt(this.schemaVersion));
+		MapLike<T> map = ops.getMap(dynamic.getValue()).resultOrPartial(Legacyskins.LOGGER::error).orElseThrow();
+		T t = ops.mergeToMap(newDynamic.getValue(), map).resultOrPartial(Legacyskins.LOGGER::error).orElseThrow();
+		return new Dynamic<>(ops, t);
+	}
+
+	public <T> T addSchemaVersion(DynamicOps<T> ops, T value) {
+		return addSchemaVersion(new Dynamic<>(ops, value)).getValue();
+	}
+}
--- src/test/java/io/github/redrain0o0/legacyskins/schema/MigratorTest.java
+++ src/test/java/io/github/redrain0o0/legacyskins/schema/MigratorTest.java
@@ -7,25 +7,25 @@ import com.mojang.serialization.JsonOps;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class LegacySkinsDataFixerTest {
+public class MigratorTest {
 
 	@Test
 	void dataFix() throws Throwable{
 
 		Assertions.assertThrows(UnsupportedOperationException.class, () -> {
 			Dynamic<JsonElement> jsonElementDynamic = new Dynamic<>(JsonOps.INSTANCE, load("999.json"));
-			LegacySkinsDataFixer.CONFIG_FIXER.fix(jsonElementDynamic);
+			Migrator.CONFIG_FIXER.fix(jsonElementDynamic);
 		});
 		{
 			Dynamic<JsonElement> jsonElementDynamic = new Dynamic<>(JsonOps.INSTANCE, load("1001.json"));
-			jsonElementDynamic = LegacySkinsDataFixer.CONFIG_FIXER.fix(jsonElementDynamic);
+			jsonElementDynamic = Migrator.CONFIG_FIXER.fix(jsonElementDynamic);
 			Assertions.assertEquals("default", jsonElementDynamic.get("skinsScreen").asString().result().orElse(null));
 			Assertions.assertFalse(jsonElementDynamic.getValue().getAsJsonObject().has("skin"));
 			Assertions.assertTrue(jsonElementDynamic.getValue().getAsJsonObject().has("currentSkin"));
 		}
 		{
 			Dynamic<JsonElement> jsonElementDynamic = new Dynamic<>(JsonOps.INSTANCE, load("1001-without-skin.json"));
-			jsonElementDynamic = LegacySkinsDataFixer.CONFIG_FIXER.fix(jsonElementDynamic);
+			jsonElementDynamic = Migrator.CONFIG_FIXER.fix(jsonElementDynamic);
 			Assertions.assertFalse(jsonElementDynamic.getValue().getAsJsonObject().has("skin"));
 			Assertions.assertFalse(jsonElementDynamic.getValue().getAsJsonObject().has("currentSkin"));
 		}
@@ -33,7 +33,7 @@ public class LegacySkinsDataFixerTest {
 
 	private JsonElement load(String str) throws Throwable {
 		Gson gson = new Gson();
-		String s = new String(LegacySkinsDataFixerTest.class.getResourceAsStream("/datafix/" + str).readAllBytes());
+		String s = new String(MigratorTest.class.getResourceAsStream("/datafix/" + str).readAllBytes());
 		return gson.fromJson(s, JsonElement.class);
 	}
 }
