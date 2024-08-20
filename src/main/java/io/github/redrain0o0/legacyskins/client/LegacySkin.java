f54cdb937266d240fb6135b4788d88d66f4ff842
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-08-20 23:32:48 +1000

    Update LegacySkin.java

--- src/main/java/io/github/redrain0o0/legacyskins/client/LegacySkin.java
+++ src/main/java/io/github/redrain0o0/legacyskins/client/LegacySkin.java
@@ -4,8 +4,22 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.resources.ResourceLocation;
 
-public record LegacySkin(ResourceLocation model) {
+import java.util.Optional;
+
+public record LegacySkin(ResourceLocation model, Optional<Cape> cape) {
 	public static final Codec<LegacySkin> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-			ResourceLocation.CODEC.fieldOf("model").forGetter(LegacySkin::model)
+			ResourceLocation.CODEC.fieldOf("model").forGetter(LegacySkin::model),
+			Cape.CODEC.optionalFieldOf("cape").forGetter(LegacySkin::cape)
 	).apply(instance, LegacySkin::new));
+
+	public LegacySkin(ResourceLocation model) {
+		this(model, Optional.empty());
+	}
+
+	public record Cape(ResourceLocation texture, boolean elytra) {
+		public static final Codec<Cape> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+				ResourceLocation.CODEC.fieldOf("texture").forGetter(Cape::texture),
+				Codec.BOOL.optionalFieldOf("elytra", true).forGetter(Cape::elytra)
+		).apply(instance, Cape::new));
+	}
 }
