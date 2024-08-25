c451eb6d06399c8b240757cc13828f536a6df2fa
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-08-25 19:42:01 +1000

    implement 1.21+ version utils

--- src/main/java/io/github/redrain0o0/legacyskins/util/VersionUtils.java
+++ src/main/java/io/github/redrain0o0/legacyskins/util/VersionUtils.java
@@ -5,7 +5,10 @@ import net.minecraft.resources.ResourceLocation;
 public enum VersionUtils {
 	;
 	public static ResourceLocation of(String namespace, String path) {
-		return new ResourceLocation(namespace, path);
+		//? if <1.21
+		/*return new ResourceLocation(namespace, path);*/
+		//? if >=1.21
+		return ResourceLocation.fromNamespaceAndPath(namespace, path);
 	}
 
 	public static ResourceLocation ofMinecraft(String path) {
@@ -13,6 +16,9 @@ public enum VersionUtils {
 	}
 
 	public static ResourceLocation parse(String path) {
-		return new ResourceLocation(path);
+		//? if <1.21
+		/*return new ResourceLocation(path);*/
+		//? if >=1.21
+		return ResourceLocation.parse(path);
 	}
 }
