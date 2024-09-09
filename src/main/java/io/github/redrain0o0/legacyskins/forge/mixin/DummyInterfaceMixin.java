45fc4c2a02cac3dc0479799b7da7b44bd472d629
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-09-09 20:37:47 +1000

    Update DummyInterfaceMixin.java

--- src/main/java/io/github/redrain0o0/legacyskins/forge/mixin/DummyInterfaceMixin.java
+++ src/main/java/io/github/redrain0o0/legacyskins/forge/mixin/DummyInterfaceMixin.java
@@ -1,9 +1,8 @@
 package io.github.redrain0o0.legacyskins.forge.mixin;
 
-import net.minecraft.client.Options;
 import net.minecraft.client.gui.components.events.ContainerEventHandler;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(value = {ContainerEventHandler.class}, priority = 2147483647)
-public abstract interface DummyInterfaceMixin {
+@Mixin(value = {ContainerEventHandler.class}, targets = {"net/minecraft/client/OptionInstance$SliderableValueSet"}, priority = 2147483647)
+public interface DummyInterfaceMixin {
 }
