c1fd01e00f7dd96b97ab195cd1d484c3c010bf70
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-09-14 16:18:02 +1000

    Update LegacySkinSprites.java

--- src/main/java/io/github/redrain0o0/legacyskins/util/LegacySkinSprites.java
+++ src/main/java/io/github/redrain0o0/legacyskins/util/LegacySkinSprites.java
@@ -4,8 +4,8 @@ import io.github.redrain0o0.legacyskins.Legacyskins;
 import net.minecraft.resources.ResourceLocation;
 
 public class LegacySkinSprites {
-	public static final ResourceLocation SKIN_BOX = ResourceLocation.tryBuild(Legacyskins.MOD_ID, "tiles/skin_box");
-	public static final ResourceLocation SKIN_PANEL = ResourceLocation.tryBuild(Legacyskins.MOD_ID, "tiles/skin_panel");
-	public static final ResourceLocation PANEL_FILLER = ResourceLocation.tryBuild(Legacyskins.MOD_ID, "tiles/panel_filler");
-	public static final ResourceLocation PACK_NAME_BOX = ResourceLocation.tryBuild(Legacyskins.MOD_ID, "tiles/pack_name_box");
+	public static final ResourceLocation SKIN_BOX = VersionUtils.of(Legacyskins.MOD_ID, "tiles/skin_box");
+	public static final ResourceLocation SKIN_PANEL = VersionUtils.of(Legacyskins.MOD_ID, "tiles/skin_panel");
+	public static final ResourceLocation PANEL_FILLER = VersionUtils.of(Legacyskins.MOD_ID, "tiles/panel_filler");
+	public static final ResourceLocation PACK_NAME_BOX = VersionUtils.of(Legacyskins.MOD_ID, "tiles/pack_name_box");
 }
