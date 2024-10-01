303a796ad40d26a875a2d478c6da58cde37c1700
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-10-02 00:56:07 +1000

    Update Legacyskins.java

--- src/main/java/io/github/redrain0o0/legacyskins/Legacyskins.java
+++ src/main/java/io/github/redrain0o0/legacyskins/Legacyskins.java
@@ -63,5 +63,7 @@ public class Legacyskins {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		// Load the config file here so Minecraft doesn't error during the resource reload and disable _every single resource pack_
+		lazyInstance();
 	}
 }
