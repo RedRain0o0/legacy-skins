d4bead7175c204861bee8c33515ecb8cdae2b6a8
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-12-06 16:37:02 +1100

    Create EasterEggUtils.java

---
+++ src/main/java/io/github/redrain0o0/legacyskins/client/util/EasterEggUtils.java
@@ -0,0 +1,21 @@
+package io.github.redrain0o0.legacyskins.client.util;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.LevelData;
+
+public enum EasterEggUtils {
+	;
+	@SuppressWarnings({"ConstantValue", "resource"})
+	public static boolean isHardcoreMode() {
+		LocalPlayer player = Minecraft.getInstance().player;
+		if (player == null) return false;
+		Level level = player.level();
+		if (level == null) return false;
+		LevelData levelData = level.getLevelData();
+		if (levelData == null) return false;
+		return levelData.isHardcore();
+	}
+}
