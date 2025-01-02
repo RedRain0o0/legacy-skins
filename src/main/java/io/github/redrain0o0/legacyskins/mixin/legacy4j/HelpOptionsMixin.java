e315101c2d1ef796ca7e71eec1bd639b70a356c1
Jab125 <67534807+Jab125@users.noreply.github.com>
2025-01-02 16:21:26 +1100

    added support for L4J 1.7.10

--- src/main/java/io/github/redrain0o0/legacyskins/mixin/legacy4j/HelpOptionsMixin.java
+++ src/main/java/io/github/redrain0o0/legacyskins/mixin/legacy4j/HelpOptionsMixin.java
@@ -15,6 +15,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import wily.legacy.client.screen./*$ l4joptionsscreen {*/HelpAndOptionsScreen/*$}*/;
 import wily.legacy.client.screen.RenderableVList;
 import wily.legacy.client.screen.RenderableVListScreen;
@@ -33,11 +34,16 @@ public class HelpOptionsMixin extends RenderableVListScreen {
 		LegacySkinsClient.openScreen(this);
 		ci.cancel();
 	}
-	*///?} else {
-	@Inject(method = "lambda$new$1(Lnet/minecraft/client/gui/components/Button;)V", at = @At(value = "HEAD"), cancellable = true /*? if forge {*//*, remap = false*//*?}*/)
+	*///?} else if legacy4j: <1.7.10 {
+	/*@Inject(method = "lambda$new$1(Lnet/minecraft/client/gui/components/Button;)V", at = @At(value = "HEAD"), cancellable = true /^? if forge {^//^, remap = false^//^?}^/)
 	private static void ChangeSkinButton(Button b, CallbackInfo ci) {
 		LegacySkinsClient.openScreen(Minecraft.getInstance().screen);
 		ci.cancel();
+	}
+	*///?} else {
+	@Inject(method = "lambda$new$16()Lnet/minecraft/client/gui/screens/Screen;", at = @At("HEAD"), cancellable = true)
+	private void ChangeSkinButton(CallbackInfoReturnable<Screen> cir) {
+		cir.setReturnValue(LegacySkinsClient.getSkinsScreen(this));
 	}
 	//?}
 }
--- src/main/java/io/github/redrain0o0/legacyskins/modrinth/ModrinthSkinPackCollection.java
+++ src/main/java/io/github/redrain0o0/legacyskins/modrinth/ModrinthSkinPackCollection.java
@@ -24,7 +24,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ModrinthSkinPackCollection {
-	private static final String userAgent = /*$ userAgent {*/"RedRainOoO/legacy-skins/1.3.0-beta.4.quirkbox.8+fabric+1.21.4"/*$}*/;
+	private static final String userAgent = /*$ userAgent {*/"RedRainOoO/legacy-skins/1.3.0.1+fabric+1.21.4"/*$}*/;
 	private static final String collectionUrl = "https://api.modrinth.com/v3/collection/bJ8YVFtd";
 	private static final String projectsUrl = "https://api.modrinth.com/v3/projects";
 	private static final String teamsUrl = "https://api.modrinth.com/v3/teams";
